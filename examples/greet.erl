cd47817cfd656e6936232a1d7237ff55a36cc68d
Victor Martinez <victor.martinez@migcan.com>
2011-06-26 15:43:42 +0100

    Added greet example

---
+++ examples/greet.erl
@@ -0,0 +1,10 @@
+-module(greet).
+
+-export([greet/2]).
+
+greet(male, Name) ->
+    io:format("Hello, Mr. ~s!~n", [Name]);
+greet(female, Name) ->
+    io:format("Hello, Mrs. ~s!~n", [Name]);
+greet(_, Name) ->
+    io:format("Hello, ~s!~n", [Name]).
