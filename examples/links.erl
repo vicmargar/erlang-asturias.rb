44d925219c1ecd764f52eccd1f38e10411c0bfbf
Victor Martinez <victor.martinez@migcan.com>
2011-06-27 19:02:42 +0100

    Reduced the output for the links example.

--- examples/links.erl
+++ examples/links.erl
@@ -4,19 +4,19 @@
 padre() ->
     process_flag(trap_exit, true),
     PidHijo = spawn_link(links, hijo, []),
-    io:format("Pid padre: ~p, Pid hijo: ~p~n", [self(), PidHijo]),
+    io:format("Padre: ~p, Hijo: ~p~n", [self(), PidHijo]),
     recibir_mensajes().
 
 recibir_mensajes() ->
     receive
         Msg ->
-            io:format("Padre ha recibido un mensaje: ~p~n", [Msg]),
+            io:format("Mensaje Padre: ~p~n", [Msg]),
             recibir_mensajes()
     end.
 
 hijo() ->
     receive
         Msg ->
-            io:format("Hijo ha recibido un mensaje: ~p~n", [Msg]),
+            io:format("Mensaje Hijo: ~p~n", [Msg]),
             hijo()
     end.
